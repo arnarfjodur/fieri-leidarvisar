339e9b5eb62b40181e658248bd8981a752941111
Arnar Sigurdsson <arnarfjodur@gmail.com>
2023-09-04 17:59:13 +0000

    let's go

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -6,6 +6,12 @@ import markdownItFootnote from  'markdown-it-footnote'
 export default defineConfig({
   title: "Tunglskotin heim í hérað",
   description: "Leiðarvísar fyrir vistkerfi nýsköpunar í dreifðum byggðum",
+  locales: {
+    root: {
+      label: 'Íslenska',
+      lang: 'is'
+    }
+  },
   markdown: {
     config: (md) => {
       // use more markdown-it plugins!
@@ -15,19 +21,42 @@ export default defineConfig({
   lastUpdated: true,
   themeConfig: {
     outlineTitle: 'Á þessari síðu',
+    footer: {
+      message: 'Þessi vefur er hluti af verkkefninu Að rækta vistkerfi nýsköpunar í dreifðum byggðum, sem stutt er af <a href="https://www.rannis.is/sjodir/rannsoknir/markaaetlun-um-samfelagslegar-askoranir/">Markáætlun um samfélagslegar áskoranir</a>',
+      copyright: 'Útgefandi: <a href="https://www.eastofmoon.com/">Austan mána ehf</a>'
+    },
     logo: { src: '/artwork/eom.png', width: 24, height: 24 },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Forsíða', link: '/' },
       { text: 'Efnisyfirlit', link: '/inngangur/efnisyfirlit' }
     ],
-
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: 'Leita',
+            buttonAriaLabel: 'buttonAriaLabel'
+          },
+          modal: {
+            noResultsText: 'Engar niðurstöður',
+            resetButtonTitle: 'Hreinsa',
+            footer: {
+              selectText: 'velja',
+              closeText: 'loka',
+              navigateText: 'til að ferðast'
+            }
+          }
+        }
+      }
+    },
     sidebar: [
       {
         text: 'Inngangur',
         items: [
+          { text: 'Efnisyfirlit', link: '/inngangur/efnisyfirlit' },
           { text: 'Formáli', link: '/inngangur/formali' },
-          { text: 'Efnisyfirlit', link: '/inngangur/efnisyfirlit' },          
           { text: 'Hvað er nýsköpun?', link: '/inngangur/hvad_er_nyskopun' },
           { text: 'Dreifðar byggðir', link: '/inngangur/dreifdar_byggdir' }
         ]
--- index.md
+++ index.md
@@ -16,15 +16,51 @@ hero:
 
 features:
   - title: Inngangur
-    details: Til hvers er þessi heimild gerð og hvernig má nýta hana
+    details: Hvernig má nýta hana þessa heimild. Helstu hugtök og forsendur útskýrð.
     link: /api-examples
     icon: 
       src: /artwork/intro-icon.svg
+  - title: Nálganir
+    link: /api-examples
+    details: Hugmyndir og nálganir á nýsköpun sem vísa veginn.
+    icon: 
+      src: /artwork/arrows-icon.svg
   - title: Uppskriftir
     link: /api-examples
-    details: Hagnýtar aðferðir til að styðja við nýsköpun í dreifðum byggðum
+    details: Hagnýtar aðferðir til að styðja við nýsköpun í dreifðum byggðum.
     icon: 
       src: /artwork/recipe-icon.svg
  
 ---
 
+<style scoped>
+
+  .bottom-acknowledements {
+    width:100%;
+    display: flex;
+    align-items:center;
+    justify-content:center;
+  }
+  .acknowledements {
+        text-align:center;
+        padding-top:60px;  
+  }
+
+  .sponsor-logo {
+    margin-left:auto;
+    margin-right:auto;
+    display:block;
+    width:70px;
+    margin-bottom:10px;
+  }
+
+</style>
+
+<div class="bottom-acknowledements">
+
+  <div class="acknowledements">
+        <img src="/artwork/rannis.png" class="sponsor-logo">
+        <div><h3>Stutt af <strong><a href="https://www.rannis.is/sjodir/rannsoknir/markaaetlun-um-samfelagslegar-askoranir/">Markáætlun um samfélagslegar áskoranir</a></strong></h3></div>
+  </div>
+
+</div>
